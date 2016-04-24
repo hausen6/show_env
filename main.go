efdfae14d9b1c75c2ce7b5bf30423f63ac6dccb8
hausen6 <hausen62480@gmail.com>
2016-04-24 20:16:10 +0900

    add list option

--- main.go
+++ main.go
@@ -5,20 +5,43 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+func showEnvironmentVal(env string, sep string) int {
+	environ := os.Getenv(env)
+	fmt.Fprintf(os.Stderr, "%s=\n", env)
+	for _, v := range filepath.SplitList(environ) {
+		fmt.Print(v, sep)
+	}
+	return 0
+}
+
+func showEnvironmentList() int {
+	envs := os.Environ()
+	for _, e := range envs {
+		p := strings.Split(e, "=")
+		fmt.Println(p[0])
+	}
+	return 0
+}
 
 func main() {
 	var (
-		envVar string
-		sepVar string
+		envVar  string
+		sepVar  string
+		listVar bool
 	)
 	flag.StringVar(&envVar, "v", "PATH", "search for environment variable")
 	flag.StringVar(&sepVar, "s", "\n", "separate char")
+	flag.BoolVar(&listVar, "l", false, "show list of environment variables")
 	flag.Parse()
 
-	env := os.Getenv(envVar)
-	fmt.Fprintf(os.Stderr, "%s=\n", envVar)
-	for _, v := range filepath.SplitList(env) {
-		fmt.Print(v, sepVar)
+	var exitCode int = 0
+	if listVar {
+		exitCode = showEnvironmentList()
+	} else {
+		exitCode = showEnvironmentVal(envVar, sepVar)
 	}
+	os.Exit(exitCode)
 }
